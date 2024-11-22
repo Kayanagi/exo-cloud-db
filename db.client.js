d417214b6adc9d6e7a3b4e864328b88ab21d26f6
Kayanagi <delordwesley@gmail.com>
2024-11-22 08:57:36 +0100

    modif db

--- db.client.js
+++ db.client.js
@@ -2,8 +2,11 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  'postgres://fakeurl', // TODO
-  {
+    process.env.DATABASE_NAME, // Database name
+    process.env.DATABASE_USERNAME, // User
+    process.env.DATABASE_PASSWORD, // Password{
+    {
+        host : process.env.DATABASE_HOST,
     dialectOptions: {
       ssl: {
         require: true,
